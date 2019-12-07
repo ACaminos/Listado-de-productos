61eba94aba481af7671347e9131fcfc9925277db
Ariel Caminos <caminosariel@gmail.com>
2019-12-06 22:12:22 -0300

    Create Celulares.php

---
+++ Celulares.php
@@ -0,0 +1,16 @@
+<HTML>
+<body background=" https://conocedores.com/wp-content/uploads/2019/11/appletv-27112019.jpg">
+<h1> <font color="black">Televisores LED</font></h1>
+<h3> <font color="green"> Bienvenido a la seccion de telefonia celular</font></h3>
+ 
+ <h4> <font color="green">Poseemos las siguientes marcas</font><h4>
+ 
+ <p><font color="red">• Apple</font></p>
+ <p><font color="red">• Samsung</font></p>
+ <p><font color="red">• Sony</font></p>
+ <p><font color="red">• Motorola</font></p>
+ <p><font color="red">• Asus</font></p>
+ <p><font color="red">• Xiaomi</font></p>
+
+<h4><font color="black">Volver al menu principal</font></h4>
+<a href="MenuPrincipal.php"><font color="green"</font>• • • Click aqui • • •</a>
