3bfb315ab0d9749ca05527ed56263a38ccc088f8
Ariel Caminos <caminosariel@gmail.com>
2019-12-06 22:09:43 -0300

    Create MenuPrincipal.php

---
+++ MenuPrincipal.php
@@ -0,0 +1,19 @@
+<html>
+<body background="http://k30.kn3.net/taringa/F/2/6/1/8/1/arispc/5FC.jpg">
+<h1> <font color="yellow"> Lista de productos </font></h1>
+<h3> <font color="red">Televisores LED</font> </h3>
+<a href="TV.php"><font color="green">Click aqui </font></a>
+<?php
+echo ("");
+?>
+<h3> <font color="red">Celulares </font> </h3>
+<a href="Celulares.php"><font color="green">Click aqui</font> </a>
+<?php
+echo("");
+?>
+<h3> <font color="red">Notebooks</font> </h3>
+<a href="Notebooks.php"> <font color="green">Click aqui </a>
+<?php
+echo("");
+?>
+</html>
