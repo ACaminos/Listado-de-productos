5f03a196bc3794ec2acafa62fc4de9aca112f617
Ariel Caminos <caminosariel@gmail.com>
2019-12-06 22:12:55 -0300

    Create Notebooks.php

---
+++ Notebooks.php
@@ -0,0 +1,16 @@
+<HTML>
+<body background=" https://images6.alphacoders.com/527/527964.jpg">
+<h1> <font color="white">Notebooks</font></h1>
+<h3> <font color="green"> Bienvenido a la seccion de Notebooks</font></h3>
+ 
+ <h4> <font color="green">Poseemos las siguientes marcas</font><h4>
+ 
+ <p><font color="red">• Apple</font></p>
+ <p><font color="red">• Asus</font></p>
+ <p><font color="red">• Acer</font></p>
+ <p><font color="red">• Lenovo</font></p>
+ <p><font color="red">• Sony</font></p>
+ <p><font color="red">• HP</font></p>
+
+<h4><font color="white">Volver al menu principal</font></h4>
+<a href="MenuPrincipal.php"><font color="green"</font>• • • Click aqui • • •</a>
